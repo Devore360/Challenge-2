ddc7820a252a62cfc95badadf500ba8b98e9094e
Devore360 <106493979+Devore360@users.noreply.github.com>
2022-07-06 14:12:50 -0500

    YEs

--- challenge-2/src/challenge2/feedme2.java
+++ challenge-2/src/challenge2/feedme2.java
@@ -1,11 +1,11 @@
 package challenge2;
 
 // Java programs for ArrayList, getting Random Elements, and scanner
-        import java.util.ArrayList;
-        import java.util.Random;
-        import java.util.Scanner;
-        import java.util.Collection;
-        import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class feedme2 {
     public static void main(String[] args)
@@ -16,11 +16,11 @@ public class feedme2 {
         //Making new Scanner class object, stored in the variable input
         Scanner sc = new Scanner(System.in);
 
-                System.out.println("Please enter dinner option #1");
-                answer = sc.nextLine();
-                Dinner.add(answer);
+        System.out.println("Please enter dinner option #1");
+        answer = sc.nextLine();
+        Dinner.add(answer);
 
-            }
-        }
     }
 }
+    }
+            }
