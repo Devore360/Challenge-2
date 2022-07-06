f625a6dd9d2df81d7b64cf7b6d49b93b31f01777
Devore360 <106493979+Devore360@users.noreply.github.com>
2022-07-06 14:09:10 -0500

    YEs

---
+++ challenge-1/src/challenge2/feedme2.java
@@ -0,0 +1,26 @@
+package challenge2;
+
+// Java programs for ArrayList, getting Random Elements, and scanner
+        import java.util.ArrayList;
+        import java.util.Random;
+        import java.util.Scanner;
+        import java.util.Collection;
+        import java.util.Iterator;
+
+public class feedme2 {
+    public static void main(String[] args)
+    {
+        // creating ArrayList
+        ArrayList<String> Dinner = new ArrayList<String>();
+
+        //Making new Scanner class object, stored in the variable input
+        Scanner sc = new Scanner(System.in);
+
+                System.out.println("Please enter dinner option #1");
+                answer = sc.nextLine();
+                Dinner.add(answer);
+
+            }
+        }
+    }
+}
